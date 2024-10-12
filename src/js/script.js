3a86873b8d976a683195a92ba043d82dce7fc916
HarukaIonaSao <robertaribeiro004@gmail.com>
2024-10-12 17:07:52 -0300

    [style]: (script.js) substituicao das imagens

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@ const titans = [
     {
       id: 1,
       nome: "Titã de Ataque",
-      imagem: "src/images/eren.jpg",
+      imagem: "src/images/eren.png",
       portador: "Eren Yeager",
       habilidades:
         "Habilidades de combate. Acesso às memórias de todos os eldianos e visões do futuro.",
@@ -12,7 +12,7 @@ const titans = [
     {
       id: 2,
       nome: "Titã Fundador",
-      imagem: "src/images/fundador.jpg",
+      imagem: "src/images/fundador.png",
       portador: "Grisha Yeager/Eren Yeager",
       habilidades:
         "Pode criar e controlar todos os titãs, modificar as memórias e corpos dos eldianos.",
@@ -22,7 +22,7 @@ const titans = [
     {
       id: 3,
       nome: "Titã Colossal",
-      imagem: "src/images/colossal.jpg",
+      imagem: "src/images/colossal.png",
       portador: "Bertolt Hoover/Armin Arlet",
       habilidades:
         "É enorme e pode evaporar seu corpo para liberar imensas quantidades de vapor.",
@@ -32,7 +32,7 @@ const titans = [
     {
       id: 4,
       nome: "Titã Fêmea",
-      imagem: "src/images/femea.jpg",
+      imagem: "src/images/femea.png",
       portador: "Annie Leonhart",
       habilidades:
         "Pode herdar habilidades muito mais facilmente do que qualquer um dos outros Titãs. Alta habilidade de regeneração.",
@@ -42,7 +42,7 @@ const titans = [
     {
       id: 5,
       nome: "Titã Blindado",
-      imagem: "src/images/blindado.jpg",
+      imagem: "src/images/blindado.png",
       portador: "Reiner Braun",
       habilidades:
         "Também conhecido por titã encouraçado.Ele também possui a capacidade de endurecer seus braços e pés em garras mais afiadas que podem ser usadas para escalar estruturas como as Muralhas.",
@@ -52,7 +52,7 @@ const titans = [
     {
       id: 6,
       nome: "Titã Bestial",
-      imagem: "src/images/bestial.jpg",
+      imagem: "src/images/bestial.png",
       portador: "Zeke Yeager",
       habilidades:
         "É o único além do quadrúpede capaz de falar na forma de Titã, podendo controlar titãs puros através da conversa e até mesmo transformar pessoas ao seu redor em Titãs puros",
@@ -62,7 +62,7 @@ const titans = [
     {
       id: 7,
       nome: "Titã Mandíbula",
-      imagem: "src/images/mandibula.jpg",
+      imagem: "src/images/mandibula.png",
       portador: "Marcel Galliard/Ymir/Porco Galliard/Falco",
       habilidades:
         "É o mais rápido dos Nove Titãs, tem dentes afiados e endurecidos e garras capazes de quebrar quase qualquer coisa",
@@ -72,7 +72,7 @@ const titans = [
     {
       id: 8,
       nome: "Titã Carroceiro",
-      imagem: "src/images/Carroceiro.jpg",
+      imagem: "src/images/Carroceiro.png",
       portador: "Pieck Finger",
       habilidades:
         "Conhecido também por titã quadrúpede.Grande resistência que o permite permanecer transformado por longos períodos de tempo",
@@ -82,7 +82,7 @@ const titans = [
     {
       id: 9,
       nome: "Titã Martelo de Guerra",
-      imagem: "src/images/martelo.jpg",
+      imagem: "src/images/martelo.png",
       portador: "Lala Tybur/Eren Yeager",
       habilidades:
         "Pode endurecer sua carne em várias armas e estruturas. Pode ser controlado a distância.",
